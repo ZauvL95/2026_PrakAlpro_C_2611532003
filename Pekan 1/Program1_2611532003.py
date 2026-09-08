ec00a26a671046394255ad7d9b2b38cd0d185b5e
Marvel <zauvl95@gmail.com>
2026-09-08 19:17:42 +0700

    Pekan 1

---
+++ Pekan 1/Program1_2611532003.py
@@ -0,0 +1,14 @@
+#Program Pertama
+"""
+Komentar
+dua baris atau lebih
+pertemuan 1 pratikum alpro
+"""
+
+print("Hello World")
+print()
+print("Departemen Informatika")
+print("Universitas Andalas")
+print("/ \\ // \\\\ /// \\\\\\")
+print("Program ini" + "dibuat oleh a\\n" 
++ "Marvel Teja 2611532003")
