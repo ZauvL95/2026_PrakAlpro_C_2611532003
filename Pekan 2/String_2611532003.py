8657a22de64e6e6548401decca23a64401f31c00
ZauvL <156491538+ZauvL95@users.noreply.github.com>
2026-09-10 11:08:26 +0700

    Pekan 2

---
+++ Pekan 2/String_2611532003.py
@@ -0,0 +1,15 @@
+# Buat file dengan nama string_NIM.py
+# nama variable ditambah 4 digit nim terakhir contoh: nama_1234
+# ganti programnya dan data masing2
+nama_2003 = "Marvel Teja"
+jenis_kelamin_2003 = 'L'
+alamat_2003 = """
+    Jl. Kotomerapak no 16
+    Kec. Padang barat
+    Kota padang
+"""
+agama_2003 = 'Buddha'
+print("Nama saya adalah: ", nama_2003)
+print("Jenis kelamin saya adalah: ", jenis_kelamin_2003)
+print("Alamat saya adalah: ", alamat_2003)
+print("Agama saya adalah: ", agama_2003)
